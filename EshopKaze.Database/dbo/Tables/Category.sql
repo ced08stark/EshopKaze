8936972f563a9a45a207f71e84de4a6f6b63f556
ced_stark08 <cedricngangue08@gmail.com>
2022-03-13 03:14:38 +0100

    Ajoutez des fichiers projet.

---
+++ EshopKaze.Database/dbo/Tables/Category.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Category] (
+    [Id]     INT           IDENTITY (1, 1) NOT NULL,
+    [Name]   NVARCHAR (50) NULL,
+    [UserId] INT           NULL,
+    CONSTRAINT [PK_Categorie] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Categorie_Categorie] FOREIGN KEY ([Name]) REFERENCES [dbo].[Category] ([Name]),
+    CONSTRAINT [IX_Categorie] UNIQUE NONCLUSTERED ([Name] ASC)
+);
+
---
+++ EshopKaze.Database/dbo/Tables/User.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[User] (
+    [Id]       INT           NOT NULL,
+    [Username] NVARCHAR (50) NOT NULL,
+    [Password] VARCHAR (100) NOT NULL,
+    [Fullname] NVARCHAR (50) NOT NULL,
+    [Role]     VARCHAR (50)  NOT NULL,
+    CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_User]
+    ON [dbo].[User]([Username] ASC);
+
